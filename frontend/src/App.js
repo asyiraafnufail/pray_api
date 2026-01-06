602bc2365fe158a004e738c83cd0230c6bc156ec
asyiraafnufail <asyiraaf4@gmail.com>
2026-01-06 19:36:13 +0700

    fix typo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
 
-// Import Komponen Baru
 import Header from './components/Header';
 import KeyGenerator from './components/KeyGenerator';
 import ApiPlayground from './components/ApiPlayground';
